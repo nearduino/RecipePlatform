604f3e57cae31d269fd1617d7ae7a85c9881b992
Aleksandar Radišić <aleksandar.radisic@uns.ac.rs>
2022-05-10 18:32:50 +0200

    feat: Ingredient constructor
    Added initial validation and exception

---
+++ RecipeHub/RecipeHub.Domain/Model/Exceptions/InvalidIngredientQuantityException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeHub.Domain.Model.Exceptions
+{
+    public class InvalidIngredientQuantityException : ArgumentException
+    {
+        public InvalidIngredientQuantityException() : base("Ingredient quantity must be greater than 0!")
+        {
+
+        }
+    }
+}
--- RecipeHub/RecipeHub.Domain/Model/Ingredient.cs
+++ RecipeHub/RecipeHub.Domain/Model/Ingredient.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RecipeHub.Domain.Model.Enums;
+using RecipeHub.Domain.Model.Exceptions;
 
 namespace RecipeHub.Domain.Model
 {
     public class Ingredient
     {
         public int Calories { get; set; }
-            public string Name { get; set; }
+        public string Name { get; set; }
         public int Quantity { get; set; }
         public MeasureUnit MeasureUnit { get; set; }
+
+        public Ingredient(int calories, string name, int quantity, MeasureUnit measureUnit)
+        {
+            Calories = calories;
+            Name = name;
+            Quantity = quantity;
+            MeasureUnit = measureUnit;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (Quantity <= 0) throw new InvalidIngredientQuantityException();
+        }
 
     }
 }
