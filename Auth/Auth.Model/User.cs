8f6b172e0311ac79769b85417cb66002e2994e50
djuka6 <djukicdjordje98@gmail.com>
2022-05-11 13:59:49 +0200

    User class modified for registration

--- Auth/Auth.Model/User.cs
+++ Auth/Auth.Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,19 @@ namespace recipe_hub
 {
     public class User
     {
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool IsAdmin { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
 
 
     }
