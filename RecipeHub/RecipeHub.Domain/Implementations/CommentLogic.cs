a2bbd539616f0fe8c6b6dab283e015bed33526ba
bosnjak.david <bosnjak.david@gmail.com>
2022-05-17 20:38:01 +0200

    save changes

--- RecipeHub/RecipeHub.Domain/Implementations/CommentLogic.cs
+++ RecipeHub/RecipeHub.Domain/Implementations/CommentLogic.cs
@@ -10,14 +10,9 @@ namespace RecipeHub.Domain.Implementations
 {
     public class CommentLogic : ICommentLogic
     {
-        private readonly Dictionary<Tuple<int, int>, Comment> Comments = new Dictionary<Tuple<int, int>, Comment>();
-
         public void CreateComment(int userId, int recipeId, Comment comment)
         {
-            // ovo ce morati drugacije
-            // komentar treba da bude vezan za odredjeni recept, ali takodje i za odredjenog korisnika
-            // treba iskombinovati ta dva id-a u jedan nekako
-            Comments.Add(new Tuple<int, int>(userId, recipeId), comment);
+
         }
 
         public Comment ReadComment(int userId, int recipeId)
