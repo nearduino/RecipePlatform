967a615c11ed3d054af1d7211b9231432a5cefd7
djuka6 <djukicdjordje98@gmail.com>
2022-05-17 11:51:08 +0200

    Created Model for Entity setup

---
+++ Auth/Auth.Model/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Model
+{
+    public interface IUserService
+    {
+        public IEnumerable<User> GetAll();
+
+        public void CreateNewUser(string firstName, string lastName, string userName, string email);
+        public User GetById(int id);
+    }
+}
