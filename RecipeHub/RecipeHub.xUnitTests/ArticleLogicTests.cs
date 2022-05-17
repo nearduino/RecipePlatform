628f2eff224853a89009fcbdc6034281a713b42f
bosnjak.david <bosnjak.david@gmail.com>
2022-05-17 12:05:28 +0200

    Added working unit tests for article logic

---
+++ RecipeHub/RecipeHub.xUnitTests/ArticleLogicTests.cs
@@ -0,0 +1,165 @@
+﻿using RecipeHub.Domain.Exceptions;
+using RecipeHub.Domain.Implementations;
+using RecipeHub.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RecipeHub.UnitTests
+{
+    public class ArticleLogicTests
+    {
+        [Theory]
+        [MemberData(nameof(CreateArticle_objects))]
+        public void CreateArticle_should_throw_exception(int id, Article a)
+        {
+            ArticleLogic al = new ArticleLogic();
+            bool exceptionThrown = false;
+            try
+            {
+                al.CreateArticle(id, a);
+            }
+            catch (ArticleException)
+            {
+                exceptionThrown = true;
+            }
+            Assert.False(exceptionThrown);
+        }
+
+        public static IEnumerable<object[]> CreateArticle_objects()
+        {
+            List<object[]> retVal = new List<object[]>();           
+            Article a1 = new Article(1, "Title 1", "text 1");
+            retVal.Add(new object[]
+            {
+                a1.Id, a1
+            }) ;
+
+            Article a2 = new Article(2, "Title 2", "text 2");
+            retVal.Add(new object[]
+            {
+                a2.Id, a2
+            });
+
+            Article a3 = new Article(1, "Title 3", "text 3");
+            retVal.Add(new object[]
+            {
+                a3.Id, a3
+            });
+            return retVal;
+        }
+
+        [Theory]
+        [MemberData(nameof(ReadArticle_objects))]
+        public void ReadArticle_should_throw_exception(int id)
+        {
+            ArticleLogic al = new ArticleLogic();
+            bool exceptionThrown = false;
+            try
+            {
+                al.ReadArticle(id);
+            }
+            catch (ArticleException)
+            {
+                exceptionThrown = true;
+            }
+            Assert.True(exceptionThrown);
+        }
+
+        public static IEnumerable<object[]> ReadArticle_objects()
+        {
+            List<object[]> retVal = new List<object[]>();
+            retVal.Add(new object[]
+            {
+                1
+            });
+            retVal.Add(new object[]
+            {
+                2
+            });
+            retVal.Add(new object[]
+            {
+                4
+            });
+            return retVal;
+        }
+
+        [Theory]
+        [MemberData(nameof(UpdateArticle_objects))]
+        public void UpdateArticle_should_throw_exception(int id, Article a)
+        {
+            ArticleLogic al = new ArticleLogic();
+            bool exceptionThrown = false;
+            try
+            {
+                al.UpdateArticle(id, a);
+            }
+            catch (ArticleException)
+            {
+                exceptionThrown = true;
+            }
+            Assert.True(exceptionThrown);
+        }
+
+        public static IEnumerable<object[]> UpdateArticle_objects()
+        {
+            List<object[]> retVal = new List<object[]>();
+            Article a1 = new Article(1, "Title 1e", "text 1e");
+            retVal.Add(new object[]
+            {
+                a1.Id, a1
+            });
+
+            Article a2 = new Article(2, "Title 2e", "text 2e");
+            retVal.Add(new object[]
+            {
+                a2.Id, a2
+            });
+
+            Article a3 = new Article(1, "Title 3e", "text 3e");
+            retVal.Add(new object[]
+            {
+                a3.Id, a3
+            });
+            return retVal;
+        }
+
+        [Theory]
+        [MemberData(nameof(DeleteArticle_objects))]
+        public void DeleteArticle_should_throw_exception(int id)
+        {
+            ArticleLogic al = new ArticleLogic();
+            bool exceptionThrown = false;
+            try
+            {
+                al.DeleteArticle(id);
+            }
+            catch (ArticleException)
+            {
+                exceptionThrown = true;
+            }
+            Assert.True(exceptionThrown);
+        }
+
+        public static IEnumerable<object[]> DeleteArticle_objects()
+        {
+            List<object[]> retVal = new List<object[]>();
+            retVal.Add(new object[]
+            {
+                1
+            });
+            retVal.Add(new object[]
+            {
+                2
+            });
+            retVal.Add(new object[]
+            {
+                4
+            });
+            return retVal;
+        }
+    }
+}
---
+++ RecipeHub/RecipeHub.xUnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace RecipeHub.xUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CreateArticle_should_throw_exception()
+        {
+            /*ArticleLogic al = new ArticleLogic();
+            Article a1 = new Article(1, "Title 1", "text 1");
+            bool exceptionThrown = false;
+            try
+            {
+                al.CreateArticle(1, a1);
+            }
+            catch (ArticleException)
+            {
+                exceptionThrown = true;
+            }
+            Assert.False(exceptionThrown);*/
+            Assert.True(true);
+        }
+    }
+}
