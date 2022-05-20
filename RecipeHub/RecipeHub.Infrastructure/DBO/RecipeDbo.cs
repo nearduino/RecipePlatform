f38fd9cb934986bef2b62b22de980d4685c1db0c
bosnjak.david <bosnjak.david@gmail.com>
2022-05-20 12:18:36 +0200

    Resolving merge conflicts 2

--- RecipeHub/RecipeHub.Infrastructure/DBO/RecipeDbo.cs
+++ RecipeHub/RecipeHub.Infrastructure/DBO/RecipeDbo.cs
@@ -19,7 +19,6 @@ namespace RecipeHub.Infrastructure.DBO
         public List<RecipeIngredientDbo> RecipeIngredientsDbo { get; set; }
         public uint PreparationTime { get; set; }
         public string ImgSrc { get; set; }
-<<<<<<< HEAD
         public Guid UserId { get; set; }
 
         //Objanjenje (na srpskom radi lakseg razumevanja): Entity framework ne dozvoljava tracking vise entiteta sa
@@ -110,8 +109,5 @@ namespace RecipeHub.Infrastructure.DBO
                 }
             }
         }
-=======
-        public int UserId { get; set; }
->>>>>>> main
     }
 }
